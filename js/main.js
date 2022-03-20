34812cd9627c5eeb2cf128f9939c0f383f02633a
Diogenes Miaja Perez <dmiaper@hotmail.com>
2022-03-20 14:06:25 +0100

    qw

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
--code 
+//Código fuente Jquery mediante JavaScript
+
+$(document).ready(function () {
+    //selector mediante id
+    $('#raza').hide(5000);
+    //selector mediante clase
+    $('.color').css('font-size', '32px');
+
+})
