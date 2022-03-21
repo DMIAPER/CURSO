e40335390704e2511daed868a01a3c4ba979228f
Diogenes Miaja <dmiaper@hotmail.com>
2022-03-21 12:04:26 +0100

    fase 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 //Código fuente Jquery mediante JavaScript
 
-$("document").ready(function () {});
+$("document").ready(function () {
+  /*modificar el estilo de la barra de navegacioón*/
+  $(window).on("scroll", function () {
+    let position = $("#ini").offset();
+    if ($(window).scrollTop() >= position.top) {
+      $("header").removeClass("transparente");
+    } else {
+      $("header").addClass("transparente");
+    }
+  });
+
+  /* Desplegar menú lateral */
+  $("#burger").click(function () {
+    $("#burger").toggleClass("abierto");
+    $(".menu").toggleClass("abierto");
+  });
+});
